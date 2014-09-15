0b215d3255d84ce757a317f6ee67c62fb9f435e2
fsantos <fsantos@NTB-FABIO.algar.local>
2014-09-15 16:02:58 -0300

    Alteração na classe ShoppingCart.java inclusão de Metodos

--- Introdução a Design Patterns/Aulas/aula5/parte2/ShoppingCart.java
+++ Introdução a Design Patterns/Aulas/aula5/parte2/ShoppingCart.java
@@ -10,4 +10,25 @@ public class ShoppingCart {
 	public ShoppingCart() {
 		products = new ArrayList<Product>();
 	}
+	
+	public void add(Product product){
+		products.add(product);
+	}
+	
+	public double getamount(){
+		double amount = 0.0;
+		for (Product product : products) {
+			amount += product.getPrice();
+		}
+	return amount;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "";
+		for (Product product : products) {
+			result += product.toString(); 
+		}
+		return result;
+	}
 }
