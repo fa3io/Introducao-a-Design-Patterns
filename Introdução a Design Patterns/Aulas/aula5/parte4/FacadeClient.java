a2e1ced58f13b0c3502931fcfd3dc3c04961d6d3
Fabio - Holding <fa3io.santos.alves@gmail.com>
2014-09-19 15:24:15 -0300

    Implementada a classe Facade.java que mostra o conceito do padrão Facade

---
+++ Introdução a Design Patterns/Aulas/aula5/parte4/FacadeClient.java
@@ -0,0 +1,16 @@
+package aula5.parte4;
+
+public class FacadeClient {
+	public static void main(String[] args) {
+		
+		Facade facade = new Facade();
+		
+		facade.registrar(15, "Lula Silva");
+		facade.shopping(03,15);
+		facade.shopping(01,15);
+		facade.shopping(04,15);
+		
+		
+		facade.finishShopping(15);
+	}
+}
---
+++ Introdução a Design Patterns/Aulas/aula5/parte4/ShoppingCart.java
@@ -0,0 +1,36 @@
+package aula5.parte4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Classe do carrinho de compras
+
+public class ShoppingCart {
+
+	private List<Product> products;
+	
+	public ShoppingCart() {
+		products = new ArrayList<Product>();
+	}
+	
+	public void add(Product product){
+		products.add(product);
+	}
+	
+	public double getamount(){
+		double amount = 0.0;
+		for (Product product : products) {
+			amount += product.getPrice();
+		}
+	return amount;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "";
+		for (Product product : products) {
+			result += product.toString(); 
+		}
+		return result;
+	}
+}
