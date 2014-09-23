c93625002855dde9fb649fed49c359a991f9b8e5
Fabio - Holding <fa3io.santos.alves@gmail.com>
2014-09-23 10:01:56 -0300

    Conclusão do estudo do Pattern FACADE

---
+++ Introdução a Design Patterns/Aulas/aula5/parte5/ClientWithoutFacade.java
@@ -0,0 +1,31 @@
+package aula5.parte5;
+
+public class ClientWithoutFacade {
+
+	public static void main(String[] args) {
+		
+		DataBase banco_de_dados = new DataBase();
+		
+		Customer rodrigo = new Customer(10, "Rodrigo");
+		
+		banco_de_dados.addCustomer(rodrigo);
+		
+		ShoppingCart carrinho = new ShoppingCart();
+		
+		rodrigo.setShoppingCart(carrinho);
+		
+		Product p1 = banco_de_dados.SelectProdut(1);
+		Product p2 = banco_de_dados.SelectProdut(3);
+		
+		rodrigo.getShoppingCart().add(p1);
+		rodrigo.getShoppingCart().add(p2);
+		
+		double amount = rodrigo.getShoppingCart().getamount();
+		
+		banco_de_dados.processPayment(rodrigo, amount);
+		
+		
+		
+		
+	}
+}
